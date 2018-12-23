b7210eadb115f07da500303a43469555ec5cef33
Jon Zgoda <jon@zgoda.us>
2018-12-23 13:25:57 -0600

    Implements functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  PDFJSStatic,
+  PDFPageViewport,
+  PDFRenderTask,
+  PDFDocumentProxy,
+} from 'pdfjs-dist';
+
+const PDFJS: PDFJSStatic = require('pdfjs-dist');
+
+import * as pdfjsLib from 'pdfjs-dist/build/pdf';
+pdfjsLib.GlobalWorkerOptions.workerSrc = './assets/pdf.worker.min.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'ng-pdfjs';
+export class AppComponent implements OnInit {
+  imgSrc: string;
+  imgWidth: number;
+  imgHeight: number;
+  errorMessage: string;
+
+  ngOnInit(): void {
+    PDFJS.disableWorker = true;
+    PDFJS.getDocument('./assets/tes.pdf').then(
+      (pdf: PDFDocumentProxy) => {
+        pdf.getPage(1).then(page => {
+          const scale = 1;
+          const viewport: PDFPageViewport = page.getViewport(scale);
+
+          const canvas: HTMLCanvasElement = document.createElement('canvas');
+          canvas.height = viewport.height;
+          canvas.width = viewport.width;
+
+          const context: CanvasRenderingContext2D = canvas.getContext('2d');
+          const task: PDFRenderTask = page.render({
+            canvasContext: context,
+            viewport: viewport,
+          });
+          task.then(() => {
+            this.imgWidth = canvas.width;
+            this.imgHeight = canvas.height;
+            this.imgSrc = canvas.toDataURL();
+          });
+        });
+      },
+      error => {
+        this.errorMessage = error;
+        console.log(error);
+      }
+    );
+  }
 }
